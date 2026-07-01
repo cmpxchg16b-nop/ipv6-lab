646a49b74657fe5913b16203dba54ed52d422f54
root <root@wien1.exploro.one>
2026-07-01 07:11:23 +0000

    Add ip forwarding

--- init.d/00-create-netns.sh
+++ init.d/00-create-netns.sh
@@ -6,4 +6,6 @@ for ns in pe1 p11 p12 p13 p21 p22 p23 p31 p32 p33 pe2; do
   echo create $ns
   ip netns add $ns
   ip -n $ns l set lo up
+  ip netns exec $ns sysctl -w net.ipv6.conf.default.forwarding=1
+  ip netns exec $ns sysctl -w net.ipv6.conf.all.forwarding=1
 done
