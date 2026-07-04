8f3f7744a3b9dd7e7abfb886a1fd649febc8da01
root <root@wien1.exploro.one>
2026-07-04 18:37:46 +0000

    Fix the experiment

--- test/03-flowlabel.sh
+++ test/03-flowlabel.sh
@@ -15,11 +15,8 @@
 
 # 1) nft: stamp fwmark = jhash(flowlabel) mod 3, and count per mark.
 ip netns exec pe1 nft create table ip6 testtable6
-ip netns exec pe1 nft create chain ip6 testtable6 testchain6 '{ type filter hook output priority mangle ; policy accept ; }'
-ip netns exec pe1 nft add rule ip6 testtable6 testchain6 meta mark set jhash ip6 flowlabel mod 3 seed 15345 log flags all
-# ip netns exec pe1 nft add rule ip6 testtable6 testchain6 meta mark 0 ip6 daddr 2001:db8:1:501::/64 counter
-# ip netns exec pe1 nft add rule ip6 testtable6 testchain6 meta mark 1 ip6 daddr 2001:db8:1:501::/64 counter
-# ip netns exec pe1 nft add rule ip6 testtable6 testchain6 meta mark 2 ip6 daddr 2001:db8:1:501::/64 counter
+ip netns exec pe1 nft create chain ip6 testtable6 testchain6 '{ type route hook output priority mangle ; policy accept ; }'
+ip netns exec pe1 nft add rule ip6 testtable6 testchain6 meta mark set jhash ip6 flowlabel mod 3 seed 15345
 
 # 2) policy rule: fwmark picks the steering table.
 
@@ -29,9 +26,10 @@ ip -n pe1 a add 2001:db8:3:101::/64 dev lo
 # awaiting OSPF propagation
 sleep 3
 
-ip -n pe1 -6 rule add flowlabel 0x0/0x3 table 1100
-ip -n pe1 -6 rule add flowlabel 0x0/0x3 table 1100
-ip -n pe1 -6 rule add flowlabel 0x0/0x3 table 1100
+ip -n pe1 -6 rule add from 2001:db8:3::/48 fwmark 0/0xffffffff table 1100
+ip -n pe1 -6 rule add from 2001:db8:3::/48 fwmark 1/0xffffffff table 1101
+ip -n pe1 -6 rule add from 2001:db8:3::/48 fwmark 2/0xffffffff table 1102
+ip -n pe1 -6 route flush cache
 
 # 3) per-table SRv6 inline-mode steering routes (mirrors 02-traffic-steering.sh,
 #    one table per flow-label bucket). The single End SID per path is the
@@ -47,27 +45,29 @@ ip -n pe1 -6 route add 2001:db8:1:501::/64 table 1102 encap seg6 mode inline seg
 # 0xffffa mark should be 2
 # 0xffffb mark should be 0
 # 0xffffd mark should be 1
+
 ip netns exec pe1 traceroute --flowlabel=0xffffa -s 2001:db8:3:101:: 2001:db8:1:501::
+# expected output:
+# traceroute to 2001:db8:1:501:: (2001:db8:1:501::), 30 hops max, 80 byte packets
+#  1  2001:db8:1:101::203:1 (2001:db8:1:101::203:1)  0.086 ms  0.013 ms  0.012 ms
+#  2  2001:db8:1:203::303:1 (2001:db8:1:203::303:1)  0.044 ms  0.014 ms  0.013 ms
+#  3  2001:db8:1:303::403:1 (2001:db8:1:303::403:1)  0.037 ms  0.015 ms  0.014 ms
+#  4  2001:db8:1:501:: (2001:db8:1:501::)  0.065 ms  0.020 ms  0.021 ms
+
 ip netns exec pe1 traceroute --flowlabel=0xffffb -s 2001:db8:3:101:: 2001:db8:1:501::
+# expected output:
+# traceroute to 2001:db8:1:501:: (2001:db8:1:501::), 30 hops max, 80 byte packets
+#  1  2001:db8:1:101::201:1 (2001:db8:1:101::201:1)  0.192 ms  0.021 ms  0.051 ms
+#  2  2001:db8:1:201::301:1 (2001:db8:1:201::301:1)  0.059 ms  0.013 ms  0.012 ms
+#  3  2001:db8:1:301::401:1 (2001:db8:1:301::401:1)  0.035 ms  0.014 ms  0.014 ms
+#  4  2001:db8:1:501:: (2001:db8:1:501::)  0.032 ms  0.017 ms  0.017 ms
+
 ip netns exec pe1 traceroute --flowlabel=0xffffd -s 2001:db8:3:101:: 2001:db8:1:501::
+# expected output:
+# traceroute to 2001:db8:1:501:: (2001:db8:1:501::), 30 hops max, 80 byte packets
+#  1  2001:db8:1:101::202:1 (2001:db8:1:101::202:1)  0.094 ms  0.018 ms  0.011 ms
+#  2  2001:db8:1:202::302:1 (2001:db8:1:202::302:1)  0.040 ms  0.014 ms  0.013 ms
+#  3  2001:db8:1:302::402:1 (2001:db8:1:302::402:1)  0.124 ms  0.034 ms  0.029 ms
+#  4  2001:db8:1:501:: (2001:db8:1:501::)  0.182 ms  0.098 ms  0.039 ms
 
-# 5) verify: per-mark counters + the three steering tables.
-ip netns exec pe1 nft list ruleset
-ip -n pe1 -6 route show table 1100
-ip -n pe1 -6 route show table 1101
-ip -n pe1 -6 route show table 1102
-
-# expected nft ruleset:
-# table ip6 testtable6 {
-# 	chain testchain6 {
-# 		type filter hook output priority filter; policy accept;
-# 		meta mark set jhash @nh,8,24 & 0xfffff mod 3 seed 0x3bf1
-# 		meta mark 0x00000000 ip6 daddr 2001:db8:1:501::/64 counter packets 3 bytes 312
-# 		meta mark 0x00000001 ip6 daddr 2001:db8:1:501::/64 counter packets 5 bytes 520
-# 		meta mark 0x00000002 ip6 daddr 2001:db8:1:501::/64 counter packets 2 bytes 208
-# 	}
-# }
-#
-# NB: we could skip nft entirely and match the flow label directly with an
-# ip-rule mask, e.g. `ip -6 rule add not ...`. The nft route is kept here to
-# make the jhash -> fwmark mapping observable via the counters above.
+# so, as we can see, that the first hop diverts, based on the flowlabel provided.
